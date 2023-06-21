abad65eab7bc4373f4b60aa2e8a56dded29587de
giuliano-marcus-bianco <giuliano.mbianco@gmail.com>
2023-06-21 17:33:56 -0300

    🧠 Learning: first time using React components
    🌐: creating a button using rfc (react functional component) and adding it a few times on the page with different arguments
    ------
    🇧🇷: criação de um botão utilizando rfc (react functional component) e adicionando algumas vezes na página com diferentes argumentos

--- cursos/03-comecando_na_biblioteca_react/01-primeiros_passos_na_biblioteca_react/dio/src/App.js
+++ cursos/03-comecando_na_biblioteca_react/01-primeiros_passos_na_biblioteca_react/dio/src/App.js
@@ -1,7 +1,15 @@
+import Button from "./components/button";
+
 function App() {
   return (
     <div className="App">
+      <h1>
       Olá React
+      </h1>
+      <Button title='Enter'/>
+      <Button title='Close'/>
+      <Button title='Open'/>
+      <Button title='Save'/>
     </div>
   );
 }
